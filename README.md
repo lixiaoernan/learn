b20638c8311fbeb1a275f284abf349eeaa9f56ac
lixiaoer <1763756518@qq.com>
2021-01-16 21:23:07 +0800

    IO流层次图

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ basicLen: 基础知识
 1.AddTwo:两数求和；------测试用
 2.Arr：数组
 3.GusseNum:猜数字；------练习方法使用
+4.Stream_File_IO:IO流学习
 
 structAndalgorithm: 数据结构与算法
 1.Arr:基本数据结构，数组题
--- basicLen/src/Stream_File_IO.java
+++ basicLen/src/Stream_File_IO.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Stream,File,IO流相关知识学习
+ */
 
 public class Stream_File_IO {
+    public static void main(String[] args) throws IOException {
+//        readChar();
+        readCharDoq();
+    }
+
+    /**
+     * 使用bufferedreader从控制台读取数据
+     */
+    public static void readChar() throws IOException {
+        //创建一个bufferedreader对象，使用InputstreamReader对象
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        //创建一个字符类型变量接受控制台输入的对象
+        char c = (char) bf.read();
+        //关闭bf对象
+        bf.close();
+        //打印到控制台
+        System.out.print(c);
+    }
+
+    /**
+     * 使用bufferedreader从控制台读取数据,直到按下q退出
+     */
+    public static void readCharDoq() throws IOException {
+        //创建一个bufferedreader对象，使用InputstreamReader对象
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        //创建一个字符类型变量接受控制台输入的对象
+        char c;
+        System.out.println("输入字符，直到按下去，退出");
+        //循环接受控制台输入的字符直到按下q
+        do{
+            c = (char) bf.read();
+            //打印到控制台
+            System.out.print(c);
+        }while (!(c == 'q'));
+        //关闭bf对象
+        bf.close();
+    }
 }
+
+
